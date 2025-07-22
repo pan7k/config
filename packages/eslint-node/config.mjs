74820ab759dfa02eaf50c74219ac59655e389aaa
Tomáš Pánik <tomas@panik.cc>
2025-07-22 21:36:38 +0200

    Updated configs

--- packages/eslint-node/config.mjs
+++ packages/eslint-node/config.mjs
@@ -61,7 +61,6 @@ export default ts.config([
       "@typescript-eslint/no-unsafe-argument": "warn",
       "@typescript-eslint/no-unsafe-assignment": "warn",
       "@typescript-eslint/no-unsafe-member-access": "warn",
-      "@typescript-eslint/no-unsafe-return": "warn",
       "@typescript-eslint/no-unused-vars": "warn",
     },
   },
